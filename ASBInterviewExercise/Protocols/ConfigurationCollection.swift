80674f19ef3f867dc3dd74c58e33d43fd9ded98d
Rosman Cheng <rosman.cheng@catchdesign.co.nz>
2022-05-22 08:52:27 +1200

    Added a service to configure server url and possible variables that differentiate from environments

---
+++ ASBInterviewExercise/Protocols/ConfigurationCollection.swift
@@ -0,0 +1,12 @@
+//
+//  ConfigurationCollection.swift
+//  ASBInterviewExercise
+//
+//  Created by Rosman Cheng on 22/05/22.
+//
+
+import Foundation
+
+protocol ConfigurationCollection {
+    var serverBaseUrl: String { get }
+}
---
+++ ASBInterviewExercise/Services/Configurations/Configuration.swift
@@ -0,0 +1,52 @@
+//
+//  Configuration.swift
+//  ASBInterviewExercise
+//
+//  Created by Rosman Cheng on 22/05/22.
+//
+
+import Foundation
+
+class Configuration: ConfigurationCollection {
+    
+    private let httpProtocolPrefix = "https://"
+    
+    init() {
+        guard let configurationDictionary = getPlistConfigurationsDictionary() else {
+            fatalError("cannot read configurations from Info.plist file")
+        }
+        
+        serverBaseUrl = "\(httpProtocolPrefix)\(getConfigurationWithKey(dictionary: configurationDictionary, key: InfoPlistKeys.serverBaseUrlKey))"
+    }
+    
+    var serverBaseUrl: String = ""
+    
+    private func getPlistConfigurationsDictionary() -> [String: Any]? {
+        guard let plistFilePath = Bundle.main.path(forResource: "Info", ofType: "plist") else {
+            return nil
+        }
+        
+        guard let plistDictionary = NSDictionary.init(contentsOfFile: plistFilePath) as? [String: Any] else {
+            return nil
+        }
+        
+        guard let configurationDictionary = plistDictionary[InfoPlistKeys.configurationKey] as? [String: Any] else {
+            return nil
+        }
+        
+        return configurationDictionary
+    }
+    
+    private func getConfigurationWithKey<T>(dictionary: [String: Any], key: String) -> T {
+        guard let configuration = dictionary[key] as? T else {
+            fatalError("The given key: \(key) does not exist in Info.plist")
+        }
+        
+        return configuration
+    }
+    
+    struct InfoPlistKeys {
+        static let configurationKey = "Configurations"
+        static let serverBaseUrlKey = "SERVER_BASE_URL"
+    }
+}
